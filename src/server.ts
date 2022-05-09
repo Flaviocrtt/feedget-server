c412e2c2c78e0a4abdf679aa0e8399d8558b6dc1
Flavio <flavio777crtt@hotmail.com>
2022-05-09 15:25:17 -0300

    port

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express'
 import { routes } from './routes'
 import cors from 'cors'
+import { env } from 'process'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
-console.log(process.env.PORT);
 
-app.listen(process.env.PORT || 3333, ()=>console.log("server runing"))
+app.listen(env.PORT || 3333, () => console.log("server runing"))
