9f19e01c6a5b64ed1a08b4e5f074b94b1ffdc3d5
Flavio <flavio777crtt@hotmail.com>
2022-05-09 14:16:14 -0300

    Ajustes na porta

--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,6 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+console.log(process.env.PORT);
 
-app.listen(process.env.port || 3333, ()=>console.log("server runing"))
+app.listen(process.env.PORT || 3333, ()=>console.log("server runing"))
