3cb1f440dacad2c3ff58504ba3d898e88ab63de1
Flavio <flavio777crtt@hotmail.com>
2022-05-10 08:33:47 -0300

    port

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
-import express from 'express'
-import { routes } from './routes'
-import cors from 'cors'
-import { env } from 'process'
+import express from "express";
+import cors from "cors";
+import { routes } from "./routes";
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(routes)
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-app.listen(process.env.$PORT || 3000, () => console.log("server runing"))
+app.listen(process.env.PORT || 3333, () => {
+  console.log("Server runing");
+});
