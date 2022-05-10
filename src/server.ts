85357158284b6f52b2a5dcac6f4deb803ad5fd2e
Flavio <flavio777crtt@hotmail.com>
2022-05-09 23:46:29 -0300

    port

--- src/server.ts
+++ src/server.ts
@@ -9,4 +9,4 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(env.PORT || 3333, () => console.log("server runing"))
+app.listen(process.env.PORT, () => console.log("server runing"))
